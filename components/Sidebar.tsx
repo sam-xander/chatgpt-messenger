b062fa2fe96797ad979875553930d0ef7996cd9c
Sam Xander <sam@samxander.com>
2023-02-07 13:30:56 +0000

    next auth sign out

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col h-screen p-2">
       <div className="flex-1">
@@ -10,6 +15,14 @@ function Sidebar() {
 
         {/* Map through the ChatRows */}
       </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="user profile"
+          className="w-12 h-12 mx-auto mb-2 rounded-full cursor-pointer hover:opacity-50"
+        />
+      )}
     </div>
   );
 }
